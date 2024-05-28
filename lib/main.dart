23c68ebc5f4326061ab17b81809c69ddc838a1e4
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-05-28 19:56:37 +0300

    login & sign up & onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:doctor_appp/Helper/cache_helper.dart';
 import 'package:doctor_appp/Screens/Splash_View.dart';
 import 'package:doctor_appp/Screens/doctorpage.dart';
 import 'package:doctor_appp/Screens/doctors_pagas/doctoranfwazon.dart';
@@ -9,6 +10,7 @@ import 'package:doctor_appp/Screens/doctors_pagas/doctormokwasab.dart';
 import 'package:doctor_appp/Screens/doctors_pagas/doctornesawtawled.dart';
 import 'package:doctor_appp/Screens/doctors_pagas/doctorsgalda.dart';
 import 'package:doctor_appp/Screens/doctors_pagas/doctorasnan.dart';
+import 'package:doctor_appp/Screens/homepage.dart';
 import 'package:doctor_appp/Screens/notifictions.dart';
 import 'package:doctor_appp/Screens/text_recognition.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -20,6 +22,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   WidgetsFlutterBinding.ensureInitialized();
+  await CacheHelper().init();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(const DoctorApp());
 }
@@ -32,24 +35,27 @@ class DoctorApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+        ),
       ),
       routes: {
-        "doctorpage": (context) => doctorpage(),
-        "doctorgalda": (context) => doctorgalda(),
-        "notifictions": (context) => NotificationsPage(),
-        "textrecognition": (context) => TextRecognitionPage(),
-        "doctorasnan": (context) => doctorasnan(),
-        "doctorazam": (context) => doctorazam(),
-        "doctormokwasab": (context) => doctormokwasab(),
-        "doctoratfal": (context) => doctoratfal(),
-        "doctornesawtawled": (context) => doctornesawtawled(),
-        "doctorcalb": (context) => doctorcalb(),
-        "doctoranfwazon": (context) => doctoranfwazon(),
-        "doctorbatna": (context) => doctorbatna(),
-        
+        "doctorpage": (context) => const doctorpage(),
+        "doctorgalda": (context) => const doctorgalda(),
+        "notifictions": (context) => const NotificationsPage(),
+        "textrecognition": (context) => const TextRecognitionPage(),
+        "doctorasnan": (context) => const doctorasnan(),
+        "doctorazam": (context) => const doctorazam(),
+        "doctormokwasab": (context) => const doctormokwasab(),
+        "doctoratfal": (context) => const doctoratfal(),
+        "doctornesawtawled": (context) => const doctornesawtawled(),
+        "doctorcalb": (context) => const doctorcalb(),
+        "doctoranfwazon": (context) => const doctoranfwazon(),
+        "doctorbatna": (context) => const doctorbatna(),
       },
-      home: const SplashView(),
+      home: CacheHelper().getData(key: 'isOnBoardingVisited') ?? false
+          ? const Homepage()
+          : const SplashView(),
     );
   }
 }
